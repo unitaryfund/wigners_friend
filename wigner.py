cf0a48e8440e2266ccb2daf03ee31b80fd8c3bf5
vrusso <vincentrusso1@gmail.com>
2023-08-12 13:05:40 -0400

    Adding ability to specify experimental settings (peek vs. reverse).

--- wigner.py
+++ wigner.py
@@ -1,10 +1,12 @@
-"""Extended Wigner's friend scenario."""
+"""Extended Wigner's friend scenario.
+
+References:
+    - arXiv:1907.05607
+    - https://www.wignersfriends.com/
+"""
+from typing import Optional
 import cirq
 import numpy as np
-
-
-def bitstring(bits: str) -> str:
-    return "".join("1" if e else "_" for e in bits)
 
 
 class SingleQubitUnitary(cirq.Gate):
@@ -14,7 +16,7 @@ class SingleQubitUnitary(cirq.Gate):
         self.single_qubit_gate = single_qubit_gate
 
     def _num_qubits_(self):
-        return 1
+        return cirq.num_qubits(self.single_qubit_gate)
 
     def _unitary_(self):
         return cirq.unitary(self.single_qubit_gate)
@@ -54,9 +56,17 @@ def cnot_ladder(num_qubits: int) -> cirq.Circuit:
     return circuit
 
 
-def rotation_basis() -> cirq.Circuit:
-    """Rotation to pick some basis to measure."""
-    pass
+def rotation(angle: float) -> cirq.Circuit:
+    """Rotation to pick some basis to measure.
+    
+    Distilled from Equations (23) and (24) from arXiv:1907.05607.
+    """
+    radians = 2 * np.deg2rad(angle)
+    qubit = cirq.LineQubit(0)
+
+    return cirq.Circuit(
+        cirq.Rz(rads=radians).on(qubit),
+    )
 
 
 def state_prep(sys_1: cirq.GridQubit, sys_2: cirq.GridQubit) -> cirq.Circuit:
@@ -97,15 +107,20 @@ def reverse_friend(
     )
 
 
-if __name__ == "__main__":
-    sim = cirq.Simulator()
-    num_qubits = 2
+def extended_wigner_circuit(
+        friend_setting_1: str,
+        friend_setting_2: str,
+        observer_circuit: cirq.Circuit,
+        rotation_circuit: Optional[cirq.Circuit] = None
+    ) -> cirq.Circuit:
+    """Generate the circuit for the extended Wigner's friend scenario with
+    choice of either "peek" or "reverse" friend settings."""
 
-    # For the sake of example, use a CNOT ladder circuit for U.
-    u_obs = MultiQubitUnitary(cnot_ladder(num_qubits))
-    u_b = SingleQubitUnitary(cirq.H)
+    friend_settings = ["peek", "reverse"]
+    if not set([friend_setting_1, friend_setting_2]).issubset(set(friend_settings)):
+        raise ValueError(f"Friend setting {friend_setting_1} or {friend_setting_2} not recognized.")
 
-    print("Simulate the circuit:")
+    num_qubits = cirq.num_qubits(observer_circuit)
 
     # Charlie and first part of bipartite system.
     charlie = [cirq.GridQubit(0, i) for i in range(num_qubits-1)]
@@ -115,48 +130,44 @@ if __name__ == "__main__":
     sys_2 = cirq.GridQubit(1, 0)
     debbie = [cirq.GridQubit(1, i) for i in range(1, num_qubits)]
 
+    # Define the experiment based on friend settings.
     circuit = state_prep(sys_1, sys_2)
 
-    circuit.append(peek_friend(charlie, sys_1, u_obs))
-    circuit.append(reverse_friend(debbie, sys_2, u_obs, u_b))
+    # Top portion of circuit (first friend setting).
+    if friend_setting_1 == "peek":
+        circuit.append(peek_friend(charlie, sys_1, observer_circuit))
+    else:
+        circuit.append(reverse_friend(debbie, sys_2, observer_circuit, rotation_circuit))
 
+    # Bottom portion of circuit (second friend setting).
+    if friend_setting_2 == "peek":
+        circuit.append(peek_friend(charlie, sys_1, observer_circuit))
+    else:
+        circuit.append(reverse_friend(debbie, sys_2, observer_circuit, rotation_circuit))
+
+    return circuit
+
+
+def run():
+    """Run simulation."""
+    pass
+
+
+if __name__ == "__main__":
+    sim = cirq.Simulator()
+    observer_circuit = MultiQubitUnitary(cnot_ladder(2))
+    rotation_circuit = SingleQubitUnitary(rotation(168))
+
+    circuit = extended_wigner_circuit("peek", "reverse", observer_circuit, rotation_circuit)
+    num_qubits = observer_circuit._num_qubits_()
+
+    print(f"Extended Wigner's friend circuit on {2 + num_qubits} qubits: \n")
     print(circuit)
-
-    results = sim.simulate(circuit)
-    print(results)
-
-    exit()
 
     # Run simulations.
     repetitions = 75
     print(f"Simulating {repetitions} repetitions...")
     result = cirq.Simulator().run(program=circuit, repetitions=repetitions)
 
-    a = np.array(result.measurements["a"][:, 0])
-    b = np.array(result.measurements["b"][:, 0])
-
-    # Print data.
-    print()
-    print('Results')
-    print('a:', bitstring(a))
-    print('b:', bitstring(b))
-
-    #circuit = cirq.Circuit(
-    #    # State prep: generates the 1/sqrt(3) * (|00> + |01> + |10>) state.
-    #    cirq.Ry(rads=2 * np.arccos(np.sqrt(2/3))).on(sys_1),
-    #    cirq.Ry(rads=np.pi/4).on(sys_2),
-    #    cirq.X(sys_1),
-    #    cirq.CX(sys_1, sys_2),
-    #    cirq.X(sys_1),
-    #    cirq.Ry(rads=-np.pi/4).on(sys_2),
-
-    #    # Peek friend:
-    #    u_obs.on(*(charlie + [sys_1])),
-    #    cirq.measure(sys_1, key="a"),
-
-    #    # Reverse friend:
-    #    u_obs.on(*([sys_2] + debbie)),
-    #    cirq.inverse(u_obs.on(*([sys_2] + debbie))),
-    #    u_b.on(sys_2),
-    #    cirq.measure(sys_2, key="b"),
-    #)
+    print(f"Alice outcomes: {result.histogram(key='a')}")
+    print(f"Bob outcomes: {result.histogram(key='b')}")
