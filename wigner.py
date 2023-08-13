847722528cbf985dccdbbf0cfc7b2a4ef7dc07ac
vrusso <vincentrusso1@gmail.com>
2023-08-12 21:49:29 -0400

    Adding inequality check.

--- wigner.py
+++ wigner.py
@@ -4,7 +4,6 @@ References:
     - arXiv:1907.05607
     - https://www.wignersfriends.com/
 """
-from typing import Optional
 import cirq
 import numpy as np
 
@@ -31,7 +30,7 @@ class MultiQubitUnitary(cirq.Gate):
         super(MultiQubitUnitary, self)
         self.multi_qubit_gate = multi_qubit_gate
 
-    def __pow__(self, other):
+    def __pow__(self, _):
         """Overloading this allows us to take the inverse (conjugate transpose) of our custom gate."""
         return cirq.MatrixGate(cirq.unitary(self.multi_qubit_gate).conj().T)
 
@@ -82,40 +81,56 @@ def state_prep(sys_1: cirq.GridQubit, sys_2: cirq.GridQubit) -> cirq.Circuit:
 
 
 def peek_friend(
-        charlie: cirq.GridQubit,
-        sys_1: cirq.GridQubit,
+        friend: cirq.GridQubit,
+        friend_key: str,
+        sys: cirq.GridQubit,
         u_obs: MultiQubitUnitary) -> cirq.Circuit:
-    """Peek procedure for Wigner's friend between first part of bipartite system and Charlie."""
-    return cirq.Circuit(
-        u_obs.on(*(charlie + [sys_1])),
-        cirq.measure(sys_1, key="a")
-    )
+    """Peek procedure for Wigner's friend scenario."""
+    if friend_key == "a":
+        return cirq.Circuit(
+            u_obs.on(*(friend + [sys])),
+            cirq.measure(sys, key=friend_key)
+        )
+    elif friend_key == "b":
+        return cirq.Circuit(
+            u_obs.on(*([sys] + friend)),
+            cirq.measure(sys, key=friend_key)
+        )
 
 
 def reverse_friend(
-        debbie: cirq.GridQubit, 
-        sys_2: cirq.GridQubit,
+        friend: cirq.GridQubit, 
+        friend_key: str,
+        sys: cirq.GridQubit,
         u_obs: MultiQubitUnitary,
         u_b: SingleQubitUnitary
 ) -> cirq.Circuit:
-    """Reverse procedure for Wigner's friend between second part of bipartite system and Debbie."""
-    return cirq.Circuit(
-        u_obs.on(*([sys_2] + debbie)),
-        cirq.inverse(u_obs.on(*([sys_2] + debbie))),
-        u_b.on(sys_2),
-        cirq.measure(sys_2, key="b"),
-    )
+    """Reverse procedure for Wigner's friend scenario."""
+    if friend_key == "a":
+        return cirq.Circuit(
+            u_obs.on(*(friend + [sys])),
+            cirq.inverse(u_obs.on(*(friend + [sys]))),
+            u_b.on(sys),
+            cirq.measure(sys, key=friend_key),
+        )
+    elif friend_key == "b":
+        return cirq.Circuit(
+            u_obs.on(*([sys] + friend)),
+            cirq.inverse(u_obs.on(*([sys] + friend))),
+            u_b.on(sys),
+            cirq.measure(sys, key=friend_key),
+        )
 
 
 def extended_wigner_circuit(
         observer_circuit: cirq.Circuit,
-        friend_setting_1: Optional[str] = None,
-        friend_setting_2: Optional[str] = None,
+        friend_setting_1: str,
+        friend_setting_2: str,
     ) -> cirq.Circuit:
     """Generate the circuit for the extended Wigner's friend scenario with
     choice of either "peek" or "reverse" friend settings."""
 
-    friend_settings = ["peek", "reverse_1", "reverse_2", None]
+    friend_settings = ["peek", "reverse_1", "reverse_2"]
     if not set([friend_setting_1, friend_setting_2]).issubset(set(friend_settings)):
         raise ValueError(f"Friend setting {friend_setting_1} or {friend_setting_2} not recognized.")
 
@@ -139,55 +154,60 @@ def extended_wigner_circuit(
     # Top portion of circuit (first friend setting).
     match friend_setting_1:
         case "peek":
-            circuit.append(peek_friend(charlie, sys_1, observer_circuit))
+            circuit.append(peek_friend(charlie, "a", sys_1, observer_circuit))
         case "reverse_1":
-            circuit.append(reverse_friend(debbie, sys_2, observer_circuit, reverse_1_circuit))
+            circuit.append(reverse_friend(charlie, "a", sys_1, observer_circuit, reverse_1_circuit))
         case "reverse_2":
-            circuit.append(reverse_friend(debbie, sys_2, observer_circuit, reverse_2_circuit))
+            circuit.append(reverse_friend(charlie, "a", sys_1, observer_circuit, reverse_2_circuit))
 
     # Bottom portion of circuit (second friend setting).
     match friend_setting_2:
         case "peek":
-            circuit.append(peek_friend(charlie, sys_1, observer_circuit))
+            circuit.append(peek_friend(debbie, "b", sys_2, observer_circuit))
         case "reverse_1":
-            circuit.append(reverse_friend(debbie, sys_2, observer_circuit, reverse_1_circuit))
+            circuit.append(reverse_friend(debbie, "b", sys_2, observer_circuit, reverse_1_circuit))
         case "reverse_2":
-            circuit.append(reverse_friend(debbie, sys_2, observer_circuit, reverse_2_circuit))
+            circuit.append(reverse_friend(debbie, "b", sys_2, observer_circuit, reverse_2_circuit))
 
     return circuit
 
 
-if __name__ == "__main__":
-    sim = cirq.Simulator()
-    observer_circuit = MultiQubitUnitary(cnot_ladder(2))
-
-    circuit = extended_wigner_circuit(observer_circuit, "peek", None)
-    num_qubits = observer_circuit._num_qubits_()
-
-    print(f"Extended Wigner's friend circuit on {2 + num_qubits} qubits: \n")
-    print(circuit)
-    
-    # Run simulations.
-    repetitions = 75
-    print(f"Simulating {repetitions} repetitions...")
+def expectation_value(circuit: cirq.Circuit, repetitions: int = 75) -> tuple[float, float]:
+    """Run the circuit `repetitions` times and calculate the resulting expectation value."""
     result = cirq.Simulator().run(program=circuit, repetitions=repetitions)
 
-    alice_expectation_value, bob_expectation_value = 0, 0
-    if "a" in result.measurements:
-        alice_expectation_value = sum(np.array(result.measurements["a"][:, 0])) / repetitions
-    if "b" in result.measurements:
-        bob_expectation_value = sum(np.array(result.measurements["b"][:, 0])) / repetitions
+    alice_expectation_value = sum(np.array(result.measurements["a"][:, 0])) / repetitions
+    bob_expectation_value = sum(np.array(result.measurements["b"][:, 0])) / repetitions
 
-    print(f"Alice expectation value: {alice_expectation_value}%")
-    print(f"Bob expectation value: {bob_expectation_value}%")
-
-    # alice_counts = result.histogram(key="a")
-    # bob_counts = result.histogram(key="b")
-
-    # print(f"Alice outcomes: {alice_counts}")
-    # print(f"Bob outcomes: {bob_counts}")
-
-    # alice_bitstring 
+    return alice_expectation_value, bob_expectation_value
 
 
-    #expectation_value = np.mean(find_parity(int_outcomes))
+def lf_facet_1(observer_circuit: cirq.Circuit) -> float:
+    """Genuine LF facet 1 as defined in Equation (13) from arXiv:1907.05607.
+    
+    The inequality for this facet is defined in terms of expectation values:
+        -<A_1> - <A_2> - <B_1> - <B_2> - <A_1 B_1> 
+        - 2 <A_1 B_2> - 2 <A_2 B_1> + 2 <A_2 B_2>
+        - <A_2 B_3> - <A_3 B_2> - <A_3 B_3> - 6 <= 0
+
+    where the sub-indices define the friend settings used, namely:
+        1. Peek
+        2. Reverse 1
+        3. Reverse 2
+    """
+    a_1, b_1 = expectation_value(extended_wigner_circuit(observer_circuit, "peek", "peek"))
+    a_2, b_2 = expectation_value(extended_wigner_circuit(observer_circuit, "reverse_1", "reverse_1"))
+    a_3, b_3 = expectation_value(extended_wigner_circuit(observer_circuit, "reverse_2", "reverse_2"))
+
+    facet = -a_1 - a_2 - b_1 - b_2 - a_1 * b_1 \
+        - 2 * a_1 * b_2 - 2 * a_2 * b_1 + 2 * a_2 * b_2 \
+        - a_2 * b_3 - a_3 * b_2 - a_3 * b_3 - 6
+
+    # Inequality is violated if the facet equation is <= 0.
+    is_violated = facet <= 0
+
+    return is_violated
+
+if __name__ == "__main__":
+    observer_circuit = MultiQubitUnitary(cnot_ladder(2))
+    print(f"Is inequality violated: {lf_facet_1(observer_circuit)}")
